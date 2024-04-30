ed04e934835abe3ea9ab7b330af8575e4b7541af
Hbourmaud <hugo.bourmaud@ynov.com>
2024-04-30 14:33:59 +0200

    wip: add inputManager

---
+++ timeless_traverse/timeless_traverse/Managers/InputManager.cpp
@@ -0,0 +1,32 @@
+﻿#include "InputManager.h"
+
+InputManager::InputManager() = default;
+
+InputManager::~InputManager() = default;
+
+void InputManager::HandleInput(sf::Event event)
+{
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) || sf::Keyboard::isKeyPressed(sf::Keyboard::Q))
+    {
+        // left
+    }
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) || sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+    {
+        // right
+
+    }
+
+    if (event.type == sf::Event::KeyReleased)
+    {
+        if (event.key.code == sf::Keyboard::Up || event.key.code == sf::Keyboard::Space)
+        {
+            // jump
+        }
+
+        if (event.key.code == sf::Keyboard::F)
+        {
+            // action
+        }
+    }
+}
---
+++ timeless_traverse/timeless_traverse/Managers/InputManager.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include <SFML/Window/Event.hpp>
+
+#include "../GameObject.h"
+
+class InputManager : GameObject
+{
+public:
+    static InputManager& GetInstance()
+    {
+        static InputManager instance;
+        return instance;
+    }
+
+    void HandleInput(sf::Event event);
+
+private:
+    InputManager();
+    ~InputManager();
+
+    InputManager(const InputManager&) = delete;
+    InputManager& operator=(const InputManager&) = delete;
+    
+};
