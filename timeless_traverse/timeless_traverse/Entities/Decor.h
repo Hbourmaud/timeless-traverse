fd48b10741931e94dc7ef439fab47448701ed22e
Hbourmaud <hugo.bourmaud@ynov.com>
2024-05-07 07:40:51 +0200

    feat: add Decor entity

---
+++ timeless_traverse/timeless_traverse/Entities/Decor.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#include "Entity.h"
+#include "../Components/SpriteComponent.h"
+
+class Decor : public Entity
+{
+public:
+    Decor();
+    ~Decor();
+
+    SpriteComponent* spriteComponent;
+};
