c6035ff5a05534c3ed2c1e32474e5d67bb557648
Hbourmaud <hugo.bourmaud@ynov.com>
2024-05-07 13:55:58 +0200

    wip: add Widget Entity
    + add basic font

---
+++ timeless_traverse/timeless_traverse/Entities/Widget.cpp
@@ -0,0 +1,42 @@
+﻿#include <filesystem>
+#include <iostream>
+
+#include "Widget.h"
+
+Widget::Widget()
+{
+    spriteComponent = new SpriteComponent();
+
+    if (!font.loadFromFile(std::filesystem::current_path().generic_string() + "/Asset/Font/arial.ttf"))
+    {
+        throw "Failed to load font";
+    }
+
+    m_text = new sf::Text();
+    m_shape = new sf::RectangleShape();
+}
+
+Widget::~Widget() = default;
+
+void Widget::setText(std::string text, sf::Color color, int size) const
+{
+    m_text->setString(text);
+    m_text->setFillColor(color);
+    m_text->setCharacterSize(size);
+    m_text->setFont(font);
+}
+
+void Widget::setShape(sf::Shape* shape)
+{
+    m_shape = shape;
+    m_shape->setPosition(m_text->getPosition());
+}
+
+void Widget::draw(sf::RenderWindow* window)
+{
+    window->draw(*m_shape);
+    window->draw(*m_text);
+}
+
+
+
---
+++ timeless_traverse/timeless_traverse/Entities/Widget.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include "Entity.h"
+#include "../Components/SpriteComponent.h"
+
+class Widget : public Entity
+{
+public:
+    Widget();
+    ~Widget();
+
+    void setText(std::string text, sf::Color color = sf::Color::White, int size = 50) const;
+    void setShape(sf::Shape* shape);
+
+    void draw(sf::RenderWindow* window);
+
+    SpriteComponent* spriteComponent;
+
+private:
+    sf::Text* m_text;
+    sf::Shape* m_shape;
+    sf::Font font;
+
+};
