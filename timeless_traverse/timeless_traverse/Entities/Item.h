a986daa964943088c4b6fb1e7873098e36aad06d
Hbourmaud <hugo.bourmaud@ynov.com>
2024-05-07 07:46:46 +0200

    feat: add Item entity

---
+++ timeless_traverse/timeless_traverse/Entities/Item.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include "Entity.h"
+#include "../Components/CollisionComponent.h"
+#include "../Components/SpriteComponent.h"
+
+class Item : Entity
+{
+public:
+    Item();
+    ~Item();
+
+    CollisionComponent* collisionComponent;
+    SpriteComponent* spriteComponent;
+};
